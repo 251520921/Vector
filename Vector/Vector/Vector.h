905984d62d88b6cbbd9b0b20430e642a33c93ae1
251520921 <251520921@qq.com>
2021-10-17 13:43:17 +0800

    新增了正序traverse遍历和逆序遍历retraverse

--- Vector/Vector/Vector.h
+++ Vector/Vector/Vector.h
@@ -50,6 +50,11 @@ public:
 	Rank binSerachA(T const& e) const;
 	Rank binSerachB(T const& e, Rank lo, Rank hi) const;
 	Rank binSerachB(T const& e) const;
+	//遍历
+	void traverse(void (*visit)(T&));	//正序遍历
+	template<class VST> void traverse(VST& visit);	//正序遍历
+	void retraverse(void (*visit)(T&));	//逆序遍历
+	template<class VST> void retraverse(VST& visit);	//逆序遍历
 };
 
 template<class T>
@@ -293,4 +298,28 @@ Rank Vector<T>::binSerachB(T const& e, Rank lo, Rank hi) const {
 template<class T>
 Rank Vector<T>::binSerachB(T const& e) const {
 	return binSerachB(e, 0, _size);
+}
+
+template<class T>
+void Vector<T>::traverse(void (*visit)(T&)) {
+	for (int i = 0; i < _size; i++)
+		visit(_elem[i]);
+}
+
+template<class T> template<class VST>
+void Vector<T>::traverse(VST& visit) {
+	for (int i = 0; i < _size; i++)
+		visit(_elem[i]);
+}
+
+template<class T>
+void Vector<T>::retraverse(void (*visit)(T&)) {
+	for (int i = _size - 1; i >= 0; i--)
+		visit(_elem[i]);
+}
+
+template<class T> template<class VST>
+void Vector<T>::retraverse(VST& visit) {
+	for (int i = _size - 1; i >= 0; i--)
+		visit(_elem[i]);
 }
